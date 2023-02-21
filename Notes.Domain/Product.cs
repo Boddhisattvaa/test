f4b2d9788883a0f5983a7ac26e013ff810abe975
Dastan Kudaybergenov <sotaimouto@gmail.com>
2023-02-21 21:16:43 +0600

    changeProduct

--- Notes.Domain/Product.cs
+++ Notes.Domain/Product.cs
@@ -3,12 +3,17 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Namaer { private get; set; } 
+        public string Namaer { private get; set; }
         public string Description { private get; set; }
         public string Category { get; set; }
         public string CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
+        public string DescriptionId { get; set; }
+        = string.Empty;
+        public string DescriptionName { get; set; }
+
+        public string DescriptionDescription { get; set; } = string.Empty;
 
     }
 }
