98209f4e65389e525849dcef8093cc629c94c997
Dastan Kudaybergenov <sotaimouto@gmail.com>
2023-02-21 21:15:25 +0600

    price

---
+++ Notes.Domain/Price.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Domain
+{
+    internal class Price
+    {
+        public int Id
+        {
+            get
+                ; set;
+        }
+        public int Title { get; set; }
+
+    }
+}
